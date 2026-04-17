a2296fd247e3ea1b6f7c2df2d2bdecd9fe333f61
Kennykelvinjr <kennykelvin108@gmail.com>
2026-04-17 13:55:59 +0300

    First start: running applications

---
+++ backend/src/main/java/com/example/backend/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend connected successfully";
+    }
+}
